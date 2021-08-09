0f1bf5af8102eba4ad1350e2a63cedec0fff2747
kader hammouya <kaderderk98@gmail.com>
2021-08-09 17:38:53 +0100

    display author of article on cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,18 +10,18 @@ home
 <div class="row my-5">
     <div class="col-md-8">
         @if (session()->has('success'))
-            <div class="alert alert-success">
-                {{session()->get('success')}}
-            </div>
+        <div class="alert alert-success">
+            {{session()->get('success')}}
+        </div>
         @endif
         <div class="row">
             @foreach ($posts as $post)
             <div class="col-md-4 mb-4">
                 <div class="card h-100">
-                    <img src="{{asset('./uploads/'.$post->image)}}"
-                        class="card-img-top" alt="...">
+                    <img src="{{asset('./uploads/'.$post->image)}}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{$post->title}}</h5>
+                        <h6 class="card-title">{{$post->user ? $post->user->name : null}}</h6>
                         <p class="card-text">{{Str::limit($post->body,100)}}</p>
                         <a href="{{route('show.post',$post->slug)}}" class="btn btn-primary">voir..</a>
                     </div>
