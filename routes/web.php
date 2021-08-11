8209e2e33efdb7d65c2116f0cf80339e27c88fb3
kader hammouya <kaderderk98@gmail.com>
2021-08-11 17:52:00 +0100

    add table categories with routes

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,11 @@ use Illuminate\Support\Facades\Route;
 }); */
 
 Route::get('/','home@index')->name('home');
-Route::get('/post/{slug}','home@showDetail')->name('show.post');
-Route::get('/create/post','home@create')->name('create.post');
-Route::post('/add/post','home@store')->name('store.post');
-Route::get('/edit/post/{slug}', 'home@edit')->name('edit.post');
-Route::put('/update/post/{slug}','home@update')->name('update.post');
-Route::delete('/delete/post/{slug}','home@delete')->name('delete.post');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return redirect('/');
 });
+
+Route::resource('categories', 'CategoryController');
+Route::resource('posts', 'PostController');
